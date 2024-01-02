34dccda5faa820f20edaef05fbc1b24c01cdea76
Badhon Biswas <biswasbadhon896@gmail.com>
2024-01-02 16:34:04 +0600

    SEO

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-
-export default function Layout(props) {
+import {Helmet} from 'react-helmet'
+export default function Layout({children,title,description,keywords,author}) {
   return (
     <>
+    <Helmet>
+      <meta charSet="utf-8"/>
+      <meta name="description" content={description}/>
+      <meta name="keywords" content={keywords}/>
+      <meta name="author" content={author}/>
+      <title>{title}</title>
+    </Helmet>
       <Header />
-      <main className="min-h-[80vh]">{props.children}</main>
+      <main className="min-h-[80vh]">{children}</main>
       <Footer/>
     </>
   );
