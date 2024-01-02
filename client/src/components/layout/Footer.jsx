7c798b85320557843a7d20055f55d9c70aaf447c
Badhon Biswas <biswasbadhon896@gmail.com>
2024-01-02 16:15:05 +0600

    Disign

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,33 +1,40 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <>
-      <div className=" bg-gray-900">
+      <div className=" bg-slate-800">
         <div className="max-w-2xl mx-auto text-white py-3">
           <div className="text-center">
-            <h3 className="text-3xl mb-3"> Download our fitness app </h3>
+            <h3 className="text-3xl mb-3"> Download our Ecommerce app </h3>
             <p> Stay fit. All day, every day. </p>
             <div className="flex justify-center my-4">
-              <div className="flex items-center border rounded-lg px-4 w-52 mx-2">
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/888/888857.png"
-                  className="w-7 md:w-8"
-                />
-                <div className="text-left ml-3">
-                  <p className="text-xs text-gray-200">Download on </p>
-                  <p className="text-sm md:text-base"> Google Play Store </p>
-                </div>
+              <div className="flex items-center rounded-lg px-4 w-52 mx-2">
+                <Link
+                  to="/about"
+                  className="  text-white  rounded md:bg-transparent  md:p-0 "
+                  aria-current="page"
+                >
+                  About
+                </Link>
               </div>
-              <div className="flex items-center border rounded-lg px-4 py-2 w-44 mx-2">
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/888/888841.png"
-                  className="w-7 md:w-8"
-                />
-                <div className="text-left ml-3">
-                  <p className="text-xs text-gray-200">Download on </p>
-                  <p className="text-sm md:text-base"> Apple Store </p>
-                </div>
+              <div className="flex items-center  rounded-lg px-4 w-44 mx-2">
+                <Link
+                  to="/contact"
+                  className=" text-white  rounded md:bg-transparent  md:p-0 "
+                  aria-current="page"
+                >
+                  Contact
+                </Link>
+              </div>
+              <div className="flex items-center  rounded-lg px-4  w-44 mx-2">
+                <Link
+                  to="/policy"
+                  className=" text-white  rounded md:bg-transparent  md:p-0 "
+                  aria-current="page"
+                >
+                  Privacy Policy
+                </Link>
               </div>
             </div>
           </div>
@@ -47,5 +54,3 @@ export default function Footer() {
     </>
   );
 }
-
-
--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,5 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  return <h1>Headers</h1>
+  const [navbar, setNavbar] = useState("");
+  window.addEventListener("mouseup", () => {});
+  return (
+    <nav className="bg-white w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 ">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link
+          to={"/"}
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">
+            Ecommerce
+          </span>
+        </Link>
+        <div className="flex md:order-2 md:space-x-0 rtl:space-x-reverse ">
+          <Link
+            to={"/"}
+            className="text-black focus:ring-4 focus:outline-none  font-bold  rounded-lg text-lg px-4 py-2 text-center "
+          >
+            Home
+          </Link>
+          <Link
+            to={"/category"}
+            className="text-black focus:ring-4 focus:outline-none  font-bold  rounded-lg text-lg px-4 py-2 text-center "
+          >
+            Category
+          </Link>
+          <Link
+            to={"/register"}
+            className="text-black focus:ring-4 focus:outline-none  font-bold  rounded-lg text-lg px-4 py-2 text-center "
+          >
+            Register
+          </Link>
+          <Link
+            to={"/login"}
+            className="text-black focus:ring-4 focus:outline-none  font-bold  rounded-lg text-lg px-4 py-2 text-center "
+          >
+            Login
+          </Link>
+          <Link
+            to={"/cart"}
+            className="text-black focus:ring-4 focus:outline-none  font-bold  rounded-lg text-lg px-4 py-2 text-center "
+          >
+            Cart (0)
+          </Link>
+        </div>
+        <div
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-sticky"
+        ></div>
+      </div>
+    </nav>
+  );
 }
